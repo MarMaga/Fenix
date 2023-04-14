ec422eee97173d5f955eab84b0dbf14909af9666
MarMaga <celomaga@gmail.com>
2023-04-14 01:38:33 -0300

    Template incluído. Página blank criada.

---
+++ blank.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+
+<?php
+include_once '_head.php';
+?>
+
+<body class="hold-transition sidebar-mini">
+  <!-- Site wrapper -->
+  <div class="wrapper">
+    <!-- Navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="index3.html" class="brand-link">
+        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+          style="opacity: .8">
+        <span class="brand-text font-weight-light">Fênix Intranet</span>
+      </a>
+
+      <?php include_once '_menu.php'; ?>
+
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <div class="container-fluid">
+
+        </div><!-- /.container-fluid -->
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+
+
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+</body>
+
+</html>
