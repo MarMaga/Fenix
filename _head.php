2342f91b1d5f7d64ebcf4717430a8104146112fa
MarMaga <celomaga@gmail.com>
2023-04-14 02:02:00 -0300

    Página Intranet incluída. Ajustes no _head.php e no _menu.php

--- _head.php
+++ _head.php
@@ -1,3 +1,4 @@
+<link rel="icon" href="dist/img/Favicon-Fenix.gif" sizes="32x32">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- _menu.php
+++ _menu.php
@@ -1,53 +1,61 @@
-<!-- Sidebar Menu -->
-<nav class="mt-2">
-    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="intranet.php" class="brand-link">
+        <img src="dist/img/LogoFenixHoriz.png" alt="Fenix Logo" class="brand-image" style="opacity: .8">
+        <span class="brand-text font-weight-light">Intranet</span>
+    </a><!-- Sidebar Menu -->
+
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-        <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>
-                    Cadastros
-                    <i class="right fas fa-angle-left"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="index.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Clientes</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="index2.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Bancos</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="index3.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Senhas para digitação</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a href="widgets.html" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                    Propostas
-                </p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="widgets.html" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                    Sair
-                </p>
-            </a>
-        </li>
-    </ul>
-</nav>
-<!-- /.sidebar-menu -->
+            <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Cadastros
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Clientes</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index2.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Bancos</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index3.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Senhas para digitação</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                        Propostas
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                        Sair
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+</aside>
