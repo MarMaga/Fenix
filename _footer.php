ad36ef88a39062f57f33e1b00ea4a56a01a7cb1d
MarMaga <celomaga@gmail.com>
2023-04-24 12:03:05 -0300

    Ajuste de versões

--- _footer.php
+++ _footer.php
@@ -11,3 +11,8 @@
   <!-- Control sidebar content goes here -->
 </aside>
 <!-- /.control-sidebar -->
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
--- _head.php
+++ _head.php
@@ -1,9 +1,10 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>Fênix Intranet</title>
-<!-- Tell the browser to be responsive to screen width -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="dist/img/Favicon-Fenix.gif"/>
+<head>
+    <link rel="icon" href="dist/img/Favicon-Fenix.gif" sizes="32x32">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Fênix Intranet</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- Font Awesome -->
 <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
